d3775c3ec9f72e87ac8456c6b2d3f87c9be7067c
alejandramunoze <atm92@georgetown.edu>
2023-10-30 17:48:34 -0400

    Troubleshooting and reorganising

--- app/unemployment.py
+++ app/unemployment.py
@@ -1,15 +1,30 @@
 # Not gonna use: from getpass import getpass
 #                   API_KEY = getpass("Please input your AlphaVantage API Key: ")
-import os
-from dotenv import load_dotenv 
 
+# IMPORTS AT THE TOP
+# Modules are at python, Packages are from the community
+
+import os
+import json
+from pprint import pprint
+from statistics import mean # Challenge B
+
+
+from dotenv import load_dotenv 
+import requests # Challenge A
+from plotly.express import line # Challenge C
+
+
+#ENVIRONMNET VARIABLES AND CONSTANTS
 load_dotenv() # go look in the .env file for any env vars
 
 API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+print(API_KEY)
 
-import requests
-import json
-from pprint import pprint
+# breakpoint()
+# quit()
+
+# FUNCTIONS
 
 request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={API_KEY}"
 
@@ -38,7 +53,7 @@ print(f"{data[0]['value']}%", "as of", data[0]["date"])
 # What is the average unemployment rate for all months during this calendar year?
 # ... How many months does this cover?
 
-from statistics import mean
+
 
 this_year = [d for d in data if "2023-" in d["date"]]
 
@@ -53,7 +68,7 @@ print("NO MONTHS:", len(this_year))
 #
 # Plot a line chart of unemployment rates over time.
 
-from plotly.express import line
+
 
 dates = [d["date"] for d in data]
 rates = [float(d["value"]) for d in data]
